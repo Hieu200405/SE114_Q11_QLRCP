47ca6cc87357876e59ff8c76eec2aabc050cd513
UIT-20520387 <91862180+UIT-20520387@users.noreply.github.com>
2025-12-28 11:07:43 +0700

    FE-models: add ReviewResponse

---
+++ frontend_java/app/src/main/java/com/example/myapplication/models/ReviewResponse.java
@@ -0,0 +1,17 @@
+package com.example.myapplication.models;
+import com.google.gson.annotations.SerializedName;
+import java.util.List;
+
+public class ReviewResponse {
+    @SerializedName("code")
+    private String code;
+
+    @SerializedName("desc")
+    private String desc;
+
+    @SerializedName("data")
+    private List<Review> data; // Danh sách các Review
+
+    public String getCode() { return code; }
+    public List<Review> getData() { return data; }
+}
